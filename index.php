23cb9f650ae9bdfae0e63d7c995377413230d1f6
Marc Curry <mcurry@redhat.com>
2016-05-25 15:47:22 -0400

    Added phpinfo section.

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <html>
 <head><title>Random</title></head>
 <body>
+
 <p>I have randomly selected the number
-<?php $choice=rand(1,100); echo $choice;?>.</p>
-<p>Its square root is <?php echo sqrt($choice);?>.</p>
+<?php 
+$choice=rand(1,100); echo $choice;
+?>
+.</p>
+<p>Its square root is 
+<?php
+echo sqrt($choice);
+?>
+.</p>
+
+<p>PHP Information: 
+<?php 
+phpinfo();
+?>
+.</p>
+
 </body>
 </html>
